9be18ec2dfaa71799b58b29c89b71cf6f8b92bfa
Alen Oroz <132370979+aoroz1@users.noreply.github.com>
2023-05-08 15:09:15 +0200

    Add files via upload

--- Entitet,atribut vjezbe.sql
+++ Entitet,atribut vjezbe.sql
@@ -11,13 +11,13 @@ create table frizerskisalon(
 	kontaktbroj int
 );
 
-create table usluga(
+create table salon_usluga(
 	naziv varchar(50),
 	cijena decimal(18,2),
 	imedjelatnice varchar(50)
 );
 
-create table korisnik(
+create table salon_korisnik(
 	ime varchar(50),
 	spol varchar(50),
 	dob int
@@ -29,13 +29,13 @@ create table taksisluzba(
 	kontaktbroj int
 );
 
-create table vozac(
+create table taksi_vozac(
 	ime varchar(50),
 	prezime varchar(50),
 	kontaktbroj int
 );
 
-create table putnik(
+create table taksi_putnik(
 	ime varchar(50),
 	prezime varchar(50),
 	kontaktbroj int
@@ -47,13 +47,13 @@ create table postolar(
 	kontaktbroj int
 );
 
-create table pkorisnik(
+create table postolar_korisnik(
 	ime varchar(50),
 	kolicinaobuce int,
 	velicinaobuce int
 );
 
-create table popravak(
+create table postolar_popravak(
 	cijena decimal(18,2),
 	trajanje int,
 );
@@ -64,15 +64,68 @@ create table restoran(
 	kontaktbroj int
 );
 
-create table jelo(
+create table restoran_jelo(
 	naziv varchar(50),
 	cijena decimal(18,2),
 	nazivkategorije varchar(50)
 );
 
-create table pice(
+create table restoran_pice(
 	naziv varchar(50),
 	cijena decimal(18,2),
 	postotakalkohola decimal(18,2)
 );
 
+create table muzej(
+	naziv varchar(50),
+	lokacija varchar(50),
+	cijenakarte decimal(18,2)
+);
+
+create table muzej_izlozba(
+	naziv varchar(50),
+	brojedjela int,
+	tema varchar(50)
+);
+
+create table muzej_kustos(
+	ime varchar(50),
+	prezime varchar(50),
+	kontaktbroj int
+);
+
+create table fakultet_student(
+	ime varchar(50),
+	prezime varchar(50),
+	smjer varchar(50)
+);
+
+create table fakultet_kolegij(
+	naziv varchar(50),
+	imeprofesora_profesorice varchar(50),
+	prezimeprofesora_profesorice varchar(50)
+);
+
+create table fakultet_rok(
+	datumroka datetime,
+	brojprijavljenihstudenata int,
+	brojroka int
+);
+
+create table odvjetnik(
+	ime varchar(50),
+	prezime varchar(50),
+	kontaktbroj int
+);
+
+create table odvjetnik_klijent(
+	ime varchar(50),
+	prezime varchar(50),
+	kontaktbroj int
+);
+
+create table odvjetnik_obrana(
+	brojsuradnikakojisudjeluju int,
+	razlogobrane varchar(50),
+	kolikojeputaodvjetnikobranio int
+);
